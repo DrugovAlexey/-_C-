42b968e7fee587e55987f2f692bf16a570b331ce
Olga <panda146@yandex.ru>
2023-06-09 21:34:44 +0300

    Правильное решение примера 3, массив

---
+++ Example_3.1/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] week = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.Write("Введите номер дня недели: ");
+string weekday = Console.ReadLine();
+int daynumber = Convert.ToInt32(weekday);
+if (daynumber < 0 || daynumber > 6)
+{
+    Console.WriteLine("Нет такого дня недели!");
+
+}
+else
+{
+    Console.WriteLine(week[daynumber - 1]);
+}
