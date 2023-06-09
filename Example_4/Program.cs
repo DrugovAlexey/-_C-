184bde0d709e57a406937318b55a4482db26a860
Olga <panda146@yandex.ru>
2023-06-09 21:46:11 +0300

    четвертая задача отобразить все числа между двумя числами

---
+++ Example_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Напишите целое положительное число: ");
+string input_string1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input_string1);
+int number2 = -number1;
+while (number2 <= number1)
+{
+    Console.Write($"{number2} ");
+    number2++;  // Увеличение на единицу, -- уменьшает на единицу, % деление на целое, += прибавляет определенное число
+}
